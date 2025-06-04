63c75d9c07a6d911469cf4289d4590a28ba0bca5
Princeraj3888 <luckyraj2307@gmail.com>
2025-06-04 19:06:14 +0700

    first commit

---
+++ src/main/java/com/exploretech/explorers/ExplorersApplication.java
@@ -0,0 +1,13 @@
+package com.exploretech.explorers;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExplorersApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExplorersApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/exploretech/explorers/ExplorersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exploretech.explorers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExplorersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
