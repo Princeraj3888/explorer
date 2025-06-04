1a7c0d8ac3f97a8ac70a75e41b0c5045c38cb814
Princeraj3888 <luckyraj2307@gmail.com>
2025-06-04 19:39:09 +0700

    first commit

---
+++ src/main/java/com/exploretech/explorers/controller/FirstController.java
@@ -0,0 +1,15 @@
+package com.exploretech.explorers.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class FirstController {
+
+    @GetMapping("/first")
+    public String sendSuccessMessage(){
+        return "you are receiving message from first method of controller..";
+    }
+}
